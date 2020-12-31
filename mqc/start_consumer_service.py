b108853fd38d6a895bf2968de56bafb7b3db9ce9
bob wang <xingbo.wang@winndoo.com>
2020-12-31 15:45:25 +0800

    test

--- mqc/start_consumer_service.py
+++ mqc/start_consumer_service.py
@@ -11,13 +11,8 @@ def run():
     server, port = params['internal_ip'].split(':')
     topics = params['topics'].split(',')
     from_begin = params['from_begin']
-    pros = []
+    pros = {}
     for topic in topics:
-        read_path = "/tmp/"+topic+"_in"
-        if os.path.exists(read_path):
-            os.remove(read_path)
-        
-        # os.mkfifo(read_path)
         cmd = "docker run -e KAFKA_SERVER="+server+ \
             " -e KAFKA_SERVER_PORT="+port+ \
             " -e KAFKA_CONSUMER_TOPIC="+topic+ \
@@ -25,10 +20,13 @@ def run():
             " registry.cn-shanghai.aliyuncs.com/wangxb/kafka-consumer:v1"
         cmd = cmd.split(' ')
         p = subprocess.Popen(cmd)
-        pros.append(p)
+        pros[topic] = p
     while True:
         for p in pros:
-            print(p.stdout)
+            if (p=='jobs'):
+                print(pros[p].stdout)
+            elif(p=='status'):
+                pass
         time.sleep(10)
 
 
