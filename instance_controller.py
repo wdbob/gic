f2973baca02cf97c61f29607630eb586e676e68d
bob wang <xingbo.wang@winndoo.com>
2021-01-04 16:10:14 +0800

    test

--- instance_controller.py
+++ instance_controller.py
@@ -1,39 +1,92 @@
+#!/usr/bin/python  
+# -*- coding: utf-8 -*
+
 from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.cvm.v20170312 import cvm_client, models
+import json
 
 
 class InstanceController:
-    def __init__(self):
-        pass
+    def __init__(self, id, key, instance):
+        self.instance = instance
+        self.status = "UNKNOWN"
+        try:
+            cred = credential.Credential(id, key) 
+            httpProfile = HttpProfile()
+            httpProfile.endpoint = "cvm.tencentcloudapi.com"
+            clientProfile = ClientProfile()
+            clientProfile.httpProfile = httpProfile
+            client = cvm_client.CvmClient(cred, "ap-shanghai", clientProfile)
+            self.client = client
+        except TencentCloudSDKException as err:
+            print(err)
+        
 
     def start(self):
-try: 
-    cred = credential.Credential(params['id'], params['key']) 
-    httpProfile = HttpProfile()
-    httpProfile.endpoint = "cvm.tencentcloudapi.com"
+        if self.client and self.status!='PENDING' and self.status!='RUNNING':
+            try: 
+                req = models.StartInstancesRequest()
+                params = {
+                    "InstanceIds": [self.instance]
+                }
+                req.from_json_string(json.dumps(params))
 
-    clientProfile = ClientProfile()
-    clientProfile.httpProfile = httpProfile
-    client = cvm_client.CvmClient(cred, "ap-shanghai", clientProfile) 
+                resp = self.client.StartInstances(req) 
+                #print(resp.to_json_string()) 
 
-    req = models.StartInstancesRequest()
-    params = {
-        "InstanceIds": [ "ins-1z5cejjd" ]
-    }
-    req.from_json_string(json.dumps(params))
+            except TencentCloudSDKException as err: 
+                print(err)
+        else:
+            print('请重新实例化InstanceController!')
 
-    resp = client.StartInstances(req) 
-    print(resp.to_json_string()) 
 
-except TencentCloudSDKException as err: 
-    print(err)
-        pass
+    def stop(self):
+        if self.client and self.status!='PENDING' and self.status!='STOPPED':
+            try: 
+                req = models.StopInstancesRequest()
+                params = {
+                    "InstanceIds": [self.instance]
+                }
+                req.from_json_string(json.dumps(params))
+                resp = self.client.StopInstances(req) 
+                print(resp.to_json_string()) 
 
-    def shutdown(self):
-        pass
+            except TencentCloudSDKException as err: 
+                print(err)
+        else:
+            print('请重新实例化InstanceController!')
 
     def is_alive(self, ins):
         pass
+
+    def check_status(self):
+        if self.client:
+            try:
+                req = models.DescribeInstancesStatusRequest()
+                params = {
+                    "InstanceIds": [ self.instance ]
+                }
+                req.from_json_string(json.dumps(params))
+
+                resp = self.client.DescribeInstancesStatus(req) 
+                # print(resp.to_json_string()) 
+                info = json.loads(resp.to_json_string())
+                self.status = info["InstanceStatusSet"][0]["InstanceState"]
+                print(self.status)
+
+            except TencentCloudSDKException as err: 
+                self.status = "UNKNOWN"
+                print(err) 
+        else:
+            self.status = "UNKNOWN"
+            print('请重新实例化InstanceController!')
+
+if __name__ == "__main__":
+    with open('config.json', 'r') as f:
+        params = json.load(f)
+    ic = InstanceController(params['id'], params['key'], params['instance'])
+    ic.stop()
+    ic.check_status()
--- mqc/start_mq_service.py
+++ mqc/start_mq_service.py
@@ -4,16 +4,21 @@ import subprocess
 import time
 
 def run():
-    print(os.path.dirname(__file__))
     json_fn = os.path.join(os.path.dirname(__file__), 'mq_config.json')
     with open(json_fn, 'r') as f:
         params = json.load(f)
         
-    cmd = "docker run --net host --name zoo -p 2181:2181 -p 2888:2888 -p 3888:3888 registry.cn-shanghai.aliyuncs.com/wangxb/kafka-zookeeper:v1"
+    cmd = "docker run --net host --restart=always --name zoo -p 2181:2181 -p 2888:2888 -p 3888:3888 registry.cn-shanghai.aliyuncs.com/wangxb/kafka-zookeeper:v1"
     cmd = cmd.split(' ')
     subprocess.Popen(cmd)
     time.sleep(20)
-    cmd = "docker run --name broker -p 9092:9092 -e EXTIP="+params['external_ip']+" -e INTIP="+params['internal_ip']+" --net host -e KAFKA_TOPICS="+params['topics']+" registry.cn-shanghai.aliyuncs.com/wangxb/kafka-broker:v1"
+
+    cmd = "docker run --name broker --restart=always -p 9092:9092 -e EXTIP="+params['external_ip']+" -e INTIP="+params['internal_ip']+" --net host -e KAFKA_TOPICS="+params['topics']+" registry.cn-shanghai.aliyuncs.com/wangxb/kafka-broker:v1"
+    cmd = cmd.split(' ')
+    subprocess.Popen(cmd)
+    time.sleep(20)
+
+    cmd = cmd = "docker run -v /tmp:/tmp --restart=always -e KAFKA_BROKER_SERVER="+params['external_ip']+" -e KAFKA_TOPICS=command --name producer-command registry.cn-shanghai.aliyuncs.com/wangxb/kafka-producer:v1"
     cmd = cmd.split(' ')
     subprocess.Popen(cmd)
 
